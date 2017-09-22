535a2eb5139c26bc86138211c174aac58d34eeeb
Julio Palbe <32197126+jujul13@users.noreply.github.com>
2017-09-22 20:43:05 +0700

    LEVEL 1 - Checking for Form Submission

--- form.php
+++ form.php
@@ -1,11 +1,6 @@
 <?php
 if(isset($_POST['submit'])){
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	
-	echo $username;
-	echo "<br />";
-	echo $password;
+	echo "Form di submit.";
 }
 ?>
 
