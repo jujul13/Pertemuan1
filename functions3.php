fde344847c17673709a1929c5047e1bc35cf5c98
Julio Palbe <32197126+jujul13@users.noreply.github.com>
2017-09-23 19:53:55 +0700

    LEVEL 2 - Delete Records from Database with PHP

---
+++ functions3.php
@@ -0,0 +1,46 @@
+<?php
+include "db.php";
+ LEVEL 2 - Delete Records from Database with PHP
+function showAllUserId(){
+		global $koneksi;
+		$query = "SELECT * FROM users";
+		$result = mysqli_query($koneksi, $query);
+		
+		if(!$result){
+		die('Query Failed' . mysqli_error($koneksi));
+	}
+	
+	while($row = mysql_fetch_assoc($result)){
+		$id = $row['id'];
+		echo "<option value='$id'>$id</option>";
+		
+	}
+}
+function updateUserTable(){
+	global $koneksi;
+	$id = $_POST['id'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	$query = "UPDATE users SET username = '$username',password = '$password'
+				WHERE id = $id";
+	$result = mysqli_query($koneksi, $query);
+	
+	if(!$result){
+		die("Query Failed.");
+	}
+}
+
+function deleteUser(){
+	global $koneksi;
+	$id = $_POST['id'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	$query = "DELETE FROM users WHERE id = $id";
+	$result = mysqli_query($koneksi, $query);
+	
+	if(!$result){
+		die("Query Failed.");
+	}
+}
